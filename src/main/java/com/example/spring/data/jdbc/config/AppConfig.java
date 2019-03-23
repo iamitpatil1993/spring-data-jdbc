b40bef4a7ddebe77ebb0bd92e1fad5f1908a4ede
Amit Patil <amipatil@athenahealth.com>
2019-03-23 11:15:41 +0530

    Chap::10:Refactoring

--- src/main/java/com/example/spring/data/jdbc/config/AppConfig.java
+++ src/main/java/com/example/spring/data/jdbc/config/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
@@ -33,7 +34,9 @@ public class AppConfig {
 	 * @return
 	 */
 	@Bean
-	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+	public JdbcOperations jdbcTemplate(DataSource dataSource) {
 		return new JdbcTemplate(dataSource);
 	}
+	
+	
 }
